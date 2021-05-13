d00ae5edfbc7d94c049f6487ce2fa36533d5ed2d
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-13 15:56:29 +0200

    feat: add and style icon to Countdown component (from semantic-ui-css)

--- src/Countdown.jsx
+++ src/Countdown.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import "./Countdown.css";
+import "./../node_modules/semantic-ui-css/semantic.css";
 
 const Countdown = ({ name, hour, minute }) => {
   return (
     <div className="countdown">
       <strong>{name}</strong> - {hour}:{minute}
+      <div className="countdown__icons">
+        <i className="icon times"></i>
+      </div>
     </div>
   );
 };
