c09b31beaf8af9a4adad7bf4961e491718fb9a78
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-13 12:08:17 +0200

    feat: refactor stateless functional component into class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import Countdown from "./Countdown";
 
-const App = () => {
-  return (
-    <div>
-      <Countdown name="śniadanie" time="07:00" />
-      <Countdown name="obiad" time="15:00" />
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Countdown name="śniadanie" time="07:00" />
+        <Countdown name="obiad" time="15:00" />
+      </div>
+    );
+  }
+}
 
 export default App;
