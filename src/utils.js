8386ba293481e73e5a3cd4a89b2ca34d6c30d23d
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-14 10:27:23 +0200

    feat: add validation of the entered hour and minute

--- src/utils.js
+++ src/utils.js
@@ -11,3 +11,13 @@ export const isValidName = (val) => {
   if (val.length > 0) return true;
   return false;
 };
+
+export const isValidHour = (val) => {
+  if (val >= 0 && val <= 23) return true;
+  return false;
+};
+
+export const isValidMinute = (val) => {
+  if (val >= 0 && val <= 59) return true;
+  return false;
+};
